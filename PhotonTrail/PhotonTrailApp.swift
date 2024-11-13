0c90f2bcd01561971612a62ec5c2e6d28ebe161e
Patrick Lai <patrick_0406@outlook.com>
2024-11-13 12:17:59 +0800

    Initial Commit

---
+++ PhotonTrail/PhotonTrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotonTrailApp.swift
+//  PhotonTrail
+//
+//  Created by Patrick Lai on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct PhotonTrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotonTrailTests/PhotonTrailTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotonTrailTests.swift
+//  PhotonTrailTests
+//
+//  Created by Patrick Lai on 2024/11/13.
+//
+
+import Testing
+@testable import PhotonTrail
+
+struct PhotonTrailTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
